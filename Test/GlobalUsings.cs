56b984093c95e64f85566bf45bd75e63fd5e10ce
Fernando Vega <58368168+normacocsub@users.noreply.github.com>
2024-02-09 23:44:18 -0500

    Create test  for services Escalera and Media

---
+++ Test/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using Domain.Services;
+global using Domain.Shared;
+global using Infrastructure.Services;
+global using Infrastructure.Shared;
+global using Microsoft.Extensions.DependencyInjection;
+global using Moq;
+global using Xunit.Abstractions;
